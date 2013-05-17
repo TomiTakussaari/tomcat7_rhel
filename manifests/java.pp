028e60128c109bdd04a432785bca38ba4734fd24
Tomi Takussaari <tomi.takussaari@elisa.fi>
2013-05-17 18:22:32 +0300

    Install oracle jdk on redhat servers

--- manifests/java.pp
+++ manifests/java.pp
@@ -1,10 +1,18 @@
 class tomcat7_rhel::java($java_version) {
   case $operatingsystem {
+    
     default: {
       package { "java-1.7.0":
         ensure => $java_version,
         name => "java-1.7.0-openjdk-devel",
       }
+    }
+    
+    redhat: {
+    	package { "java-1.7.0":
+        	ensure => latest,
+        	name => "java-1.7.0-oracle-devel"
+        }
     }
   }
 }
